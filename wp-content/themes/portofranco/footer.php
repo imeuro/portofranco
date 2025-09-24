916b646b5cdbbaac70a9d2c7c24a93c13a5e0cf7
imeuro <mauro.fioravanzi@gmail.com>
2025-09-24 12:12:16 +0200

    gtag

--- wp-content/themes/portofranco/footer.php
+++ wp-content/themes/portofranco/footer.php
@@ -31,6 +31,15 @@
         </ul>
     </div>
   </footer>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-65H72CQ48V"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-65H72CQ48V');
+</script>
   <?php wp_footer(); ?>
 </body>
 </html> 
